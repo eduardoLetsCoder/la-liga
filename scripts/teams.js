3b5904f9c80e0a61b6a355daad2d0be4751050ec
eduardo <eduardoamdev@gmail.com>
2021-05-14 00:36:48 +0200

    Código refectorizado y maquietación de la navbar terminada.

--- scripts/teams.js
+++ scripts/teams.js
@@ -2,88 +2,7 @@ let tableCaption = document.getElementById("teams-table-caption");
 let thead = document.getElementById("teams-thead");
 let tbody = document.getElementById("teams-tbody");
 
-function getTeams(whichTeams) {
-  tbody.innerHTML = "";
-  thead.innerHTML =
-    "<th>Nombre</th><th>Goles a favor</th><th>Goles en contra</th><th>Diferencia de goles</th>";
-  let url = "http://api.football-data.org/v2/competitions/2014/standings";
-  fetch(url, {
-    method: "GET",
-    headers: {
-      "X-Auth-Token": "875bb0b93478426fb1050e158da976cf",
-    },
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      let info = data.standings[0].table;
-      if (whichTeams == "all") {
-        tableCaption.innerText = "Todos los equipos de La Liga";
-        for (let i = 0; i < info.length; i++) {
-          let tr = document.createElement("tr");
-          tr.innerHTML = `<tr><td>${info[i].team.name}</td><td>${info[i].goalsFor}</td><td>${info[i].goalsAgainst}</td><td>${info[i].goalDifference}</td></tr>`;
-          tbody.appendChild(tr);
-        }
-      } else if (whichTeams == "topFiveInGoals") {
-        tableCaption.innerText = "Equipos más goleadores";
-        let infoGoals = [];
-        for (let i = 0; i < info.length; i++) {
-          let goals = info[i].goalsFor;
-          infoGoals.push(goals);
-        }
-        let sortedInfoGoals = [];
-        sortedInfoGoals = infoGoals.sort((a, b) => {
-          return b - a;
-        });
-        let firstFive = [];
-        for (let i = 0; i < 5; i++) {
-          firstFive.push(sortedInfoGoals[i]);
-        }
-        let sortedInfo = [];
-        for (let i = 0; i < firstFive.length; i++) {
-          for (let j = 0; j < info.length; j++) {
-            if (info[j].goalsFor == firstFive[i]) {
-              sortedInfo.push(info[j]);
-            }
-          }
-        }
-        for (let i = 0; i < sortedInfo.length; i++) {
-          let tr = document.createElement("tr");
-          tr.innerHTML = `<tr><td>${sortedInfo[i].team.name}</td><td>${sortedInfo[i].goalsFor}</td><td>${sortedInfo[i].goalsAgainst}</td><td>${sortedInfo[i].goalDifference}</td></tr>`;
-          tbody.appendChild(tr);
-        }
-      } else if (whichTeams == "topFiveDefenders") {
-        tableCaption.innerText = "Mejores equipos en defensa";
-        let infoGoals = [];
-        for (let i = 0; i < info.length; i++) {
-          let goals = info[i].goalsAgainst;
-          infoGoals.push(goals);
-        }
-        let sortedInfoGoals = [];
-        sortedInfoGoals = infoGoals.sort((a, b) => {
-          return a - b;
-        });
-        let firstFive = [];
-        for (let i = 0; i < 5; i++) {
-          firstFive.push(sortedInfoGoals[i]);
-        }
-        let sortedInfo = [];
-        for (let i = 0; i < firstFive.length; i++) {
-          for (let j = 0; j < info.length; j++) {
-            if (info[j].goalsAgainst == firstFive[i]) {
-              sortedInfo.push(info[j]);
-            }
-          }
-        }
-        for (let i = 0; i < sortedInfo.length; i++) {
-          let tr = document.createElement("tr");
-          tr.innerHTML = `<tr><td>${sortedInfo[i].team.name}</td><td>${sortedInfo[i].goalsFor}</td><td>${sortedInfo[i].goalsAgainst}</td><td>${sortedInfo[i].goalDifference}</td></tr>`;
-          tbody.appendChild(tr);
-        }
-      }
-    });
-}
-
-function getAverages(goalsOrDefense) {
+function getInfo(allGoalsOrDefense) {
   tbody.innerHTML = "";
   let url = "http://api.football-data.org/v2/competitions/2014/standings";
   fetch(url, {
@@ -112,7 +31,16 @@ function getAverages(goalsOrDefense) {
             teamStats.name = teams[i].team.name;
             statistics.push(teamStats);
           }
-          if (goalsOrDefense == "goals") {
+          if (allGoalsOrDefense == "all") {
+            tableCaption.innerText = "Goles de los equipos de La Liga";
+            thead.innerHTML =
+              "<th>Nombre</th><th>A favor</th><th>En contra</th><th>Diferencia</th>";
+            for (let i = 0; i < teams.length; i++) {
+              let tr = document.createElement("tr");
+              tr.innerHTML = `<tr><td>${teams[i].team.name}</td><td>${teams[i].goalsFor}</td><td>${teams[i].goalsAgainst}</td><td>${teams[i].goalDifference}</td></tr>`;
+              tbody.appendChild(tr);
+            }
+          } else if (allGoalsOrDefense == "goals") {
             tableCaption.innerText =
               "Equipos con mayor media de goles por partido";
             let sortedStatistics = [];
@@ -137,7 +65,7 @@ function getAverages(goalsOrDefense) {
               tr.innerHTML = `<tr><td>${sortedStatistics[i].name}</td><td>${sortedStatistics[i].goalsAverage}</td></tr>`;
               tbody.appendChild(tr);
             }
-          } else if (goalsOrDefense == "defense") {
+          } else if (allGoalsOrDefense == "defense") {
             tableCaption.innerText =
               "Equipos com menos goles encajados como visitante";
             thead.innerHTML =
@@ -168,7 +96,7 @@ function getAverages(goalsOrDefense) {
             }
             sortedStatistics = statistics.sort((a, b) => {
               return a.awayGoalsAgainstAverge - b.awayGoalsAgainstAverge;
-            })
+            });
             for (let i = 0; i < 5; i++) {
               let tr = document.createElement("tr");
               tr.innerHTML = `<tr><td>${sortedStatistics[i].name}</td><td>${sortedStatistics[i].awayGoalsAgainstAverge}</td></tr>`;
@@ -191,4 +119,4 @@ function round(num, decimales = 2) {
   return signo * (num[0] + "e" + (num[1] ? +num[1] - decimales : -decimales));
 }
 
-getTeams("all");
+getInfo("all");
